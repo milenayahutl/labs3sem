26630b9bf16c8aec0622c7c7f623b305e68f2333
unggi <milena.yakhutl.i@mail.ru>
2025-11-03 15:23:13 +0300

    deleted unnecessary files

--- gen_random.py
+++
@@ -1,7 +0,0 @@
-import random
-
-def gen_random(num, min, max):
-    for _ in range(num):
-        yield random.randint(min, max)
-
-print(list(gen_random(5, 1, 3)))
--- unique.py
+++
@@ -1,7 +0,0 @@
-from gen_random import gen_random
-
-class unique(object):
-    def __init__(self, items, **kwargs):
-
-
-# **kwargs — способ передать в функцию любое количество именованных аргументов (ключ=значение).
