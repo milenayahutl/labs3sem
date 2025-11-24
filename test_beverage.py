b3ced40eae8ec20af2988069b04a64a1b181bfcd
unggi <milena.yakhutl.i@mail.ru>
2025-11-25 02:31:45 +0300

    add tdd test, mock test and smth about bdd test

--- test_beverage.py
+++ test_beverage.py
@@ -1,6 +1,7 @@
 #TDD стиль: 1. написать проверку чтобы он падал
 # 2. написать минимальный код, чтобы тест прошел
 # 3. доработать код, не ломая проверки
+#для павершела достаточно просто запустить python test_beverage.py тк папка tests беда беда проблема проблема
 
 from beverage import Cappuccino, WhippedCream
 
@@ -14,7 +15,7 @@ def test_WhippedCream():
     capp = Cappuccino()
     wc = WhippedCream(capp)
     assert wc.get_description() == "Cappuccino, with whipped cream"
-    assert wc.get_price() == 2.5
+    assert wc.get_price() == 2.3
     print("Whipped cream test passed!")
 
 if __name__ == '__main__':
---
+++ test_order.py
@@ -0,0 +1,20 @@
+#мок это как бы поддельный объект, чтобы не отправлять реальные уведомления, но он запоминает, сколько раз его вызвали и как
+
+
+from unittest.mock import Mock
+from order import Order, Customer
+
+def test_order_notification():
+    order = Order("Test-001", None)
+    customer = Customer("testik")
+    customer.update = Mock()   #заменяет на фейк объект
+
+    order.subscribe(customer)
+    order.get_status("ready")
+
+    customer.update.assert_called_once() #успех если вызов был 1 раз
+    print("Mock test passed!")
+
+if __name__ == '__main__':
+    test_order_notification()
+    print("Test passed!")
